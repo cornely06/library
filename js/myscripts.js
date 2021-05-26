6e94395d0375c187c25c18c43b1d81ab3727be97
Raymond Cornely <cornely06@gmail.com>
2021-05-26 19:46:31 -0400

    Add container for books and change click event listeners to mousedown

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,10 +1,10 @@
 let myLibrary = [];
 
-function Book(title, author, pages, year, isRead) {
+function Book(title, author, year, pages, isRead) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.year = year;
+    this.pages = pages;
     this.isRead = isRead;
 }
 
@@ -20,8 +20,8 @@ function addBook(e) {
         return false;
     }
     let author = document.querySelector('#author').value;
-    let pages = document.querySelector('#pages').value;
     let year = document.querySelector('#year').value;
+    let pages = document.querySelector('#pages').value;
     const rbs = document.querySelectorAll('input[name="is-read"]');
     let isRead;
     for (const rb of rbs) {
@@ -30,7 +30,7 @@ function addBook(e) {
             break;
         }
     }
-    let book = new Book(title, author, pages, year, isRead);
+    let book = new Book(title, author, year, pages, isRead);
     addBookToLibrary(book);
     console.log(myLibrary);
     resetFields();
@@ -40,8 +40,8 @@ function addBook(e) {
 function resetFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
-    document.querySelector('#pages').value = '';
     document.querySelector('#year').value = '';
+    document.querySelector('#pages').value = '';
     document.querySelector('#unread').checked = true;
 }
 
@@ -51,20 +51,36 @@ function deleteBook() {
 }
 
 function showLibrary() {
-    let library = document.querySelector('#library');
+    let library = document.querySelector('#library-container');
     while(library.firstChild) {
         library.removeChild(library.firstChild);
     }
     myLibrary.forEach((book, index) => {
-        let listItem = document.createElement('LI');
-        let textNode = document.createTextNode(`${book.title} by ${book.author} is ${book.pages} pages, was released in ${book.year}, and ${isRead(book.isRead)}`);
-        listItem.appendChild(textNode);
-        let remove = document.createElement('BUTTON');
-        remove.innerHTML = "&times;";
-        remove.dataset.index = index;
-        remove.addEventListener('click', deleteBook);
-        listItem.appendChild(remove);
-        library.appendChild(listItem);
+        let bookContainer = document.createElement('div');
+        bookContainer.classList.add('book-container');
+        let title = document.createElement('div');
+        title.classList.add('title-container');
+        title.textContent = `${book.title}`;
+        let author = document.createElement('div');
+        author.classList.add('author-container');
+        author.textContent = `${book.author}`;
+        let year = document.createElement('div');
+        year.classList.add('year-container');
+        year.textContent = `${book.year}`;
+        let pages = document.createElement('div');
+        pages.classList.add('pages-container');
+        pages.textContent = `${book.pages}`;
+        let removeBtn = document.createElement('span');
+        removeBtn.classList.add('close');
+        removeBtn.innerHTML = "&times;";
+        removeBtn.dataset.index = index;
+        removeBtn.addEventListener('mousedown', deleteBook);
+        bookContainer.appendChild(removeBtn);
+        bookContainer.appendChild(title);
+        bookContainer.appendChild(author);
+        bookContainer.appendChild(year);
+        bookContainer.appendChild(pages);
+        library.appendChild(bookContainer);
     })
 }
 
@@ -78,20 +94,20 @@ form.addEventListener('submit', showLibrary);
 
 let modal = document.querySelector('.modal')
 let closeBtn = document.querySelector('.modal .modal-content .close');
-closeBtn.addEventListener('click', closeForm);
+closeBtn.addEventListener('mousedown', closeForm);
 
 function closeForm() {
     modal.style.display = 'none';
 }
 
 let newBookBtn = document.querySelector('#new-book-btn');
-newBookBtn.addEventListener('click', openForm);
+newBookBtn.addEventListener('mousedown', openForm);
 
 function openForm() {
     modal.style.display = 'block';
 }
 
-window.addEventListener('click', function(e) {
+window.addEventListener('mousedown', function(e) {
     if (e.target == modal) {
         modal.style.display = 'none';
     }
