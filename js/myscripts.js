274717baefa1344df1fabc25f0b369903237e0a9
Raymond Cornely <cornely06@gmail.com>
2021-05-26 00:22:48 -0400

    Add modal box for form

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,8 +1,10 @@
 let myLibrary = [];
 
-function Book(title, author, isRead) {
+function Book(title, author, pages, year, isRead) {
     this.title = title;
     this.author = author;
+    this.pages = pages;
+    this.year = year;
     this.isRead = isRead;
 }
 
@@ -13,10 +15,28 @@ function addBookToLibrary(book) {
 function addBook() {
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
-    let isRead = document.querySelector('#is-read').value;
-    let book = new Book(title, author, isRead);
+    let pages = document.querySelector('#pages').value;
+    let year = document.querySelector('#year').value;
+    const rbs = document.querySelectorAll('input[name="is-read"]');
+    let isRead;
+    for (const rb of rbs) {
+        if (rb.checked) {
+            isRead = rb.value;
+            break;
+        }
+    }
+    let book = new Book(title, author, pages, year, isRead);
     addBookToLibrary(book);
     console.log(myLibrary);
+    resetFields();
+}
+
+function resetFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#pages').value = '';
+    document.querySelector('#year').value = '';
+    document.querySelector('#unread').checked = true;
 }
 
 function deleteBook() {
@@ -31,7 +51,7 @@ function showLibrary() {
     }
     myLibrary.forEach((book, index) => {
         let listItem = document.createElement('LI');
-        let textNode = document.createTextNode(`${book.title} by ${book.author} ${isRead(book.isRead)}`);
+        let textNode = document.createTextNode(`${book.title} by ${book.author} is ${book.pages} pages, was released in ${book.year}, and ${isRead(book.isRead)}`);
         listItem.appendChild(textNode);
         let remove = document.createElement('BUTTON');
         remove.innerHTML = "&times;";
@@ -51,3 +71,24 @@ newBook.addEventListener('click', addBook);
 
 const displayLibrary = document.querySelector('#show-library');
 displayLibrary.addEventListener('click', showLibrary);
+
+let modal = document.querySelector('.modal')
+let closeBtn = document.querySelector('.modal .modal-content .close');
+closeBtn.addEventListener('click', closeForm);
+
+function closeForm() {
+    modal.style.display = 'none';
+}
+
+let newBookBtn = document.querySelector('#new-book');
+newBookBtn.addEventListener('click', openForm);
+
+function openForm() {
+    modal.style.display = 'block';
+}
+
+window.addEventListener('click', function(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+})
